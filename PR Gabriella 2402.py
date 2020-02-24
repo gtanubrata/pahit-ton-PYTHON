8d6da107277ed784677327ed47dd0511845c6805
gtanubrata <59508574+gtanubrata@users.noreply.github.com>
2020-02-24 23:45:21 +0700

    Update PR Gabriella 2402.py

--- PR Gabriella 2402.py
+++ PR Gabriella 2402.py
@@ -1,66 +1,3 @@
-#Example
-
-# A = [[1, 4, 5, 12], 
-#     [-5, 8, 9, 0],
-#     [-6, 7, 11, 19]]
-
-# print("A =", A) 
-# print("A[1] =", A[1])      # 2nd row
-# print("A[1][2] =", A[1][2])   # 3rd element of 2nd row
-# print("A[0][-1] =", A[0][-1])   # Last element of 1st Row
-
-# column = [];        # empty list
-# for row in A:
-#   column.append(row[2])   
-
-# print("3rd column =", column)
-
-
-#Rotate clockwise (kanan) test
-
-x = [[1, 2],
-     [3, 4]]
-
-x[0][0], x[1][0] = x[1][0], x[0][0]
-print(x)
-
-x[0][0], x[0][1] = x[0][1], x[0][0]
-print(x)
-
-x[0][1], x[1][1] = x[1][1], x[0][1]
-print(x)
-
-# size = 2
-# max = 1
-
-
-
-y = [[1, 2, 3],
-     [4, 5, 6],
-     [7, 8, 9]]
-
-y[0][0], y[2][0] = y[2][0], y[0][0]
-print(y)
-
-y[0][0], y[0][2] = y[0][2], y[0][0]
-print(y)
-
-y[0][2], y[2][2] = y[2][2], y[0][2]
-print(y)
-
-y[0][1], y[1][0] = y[1][0], y[0][1]
-print(y)
-
-y[0][1], y[1][2] = y[1][2], y[0][1]
-print(y)
-
-y[1][2], y[2][1] = y[2][1], y[1][2]
-print(y)
-
-# size = 3
-# max = 2
-# max - 1 = 1
-
 z = [[1, 2, 3, 4],
      [5, 6, 7, 8],
      [9, 10, 11, 12],
@@ -71,6 +8,8 @@ def print_matrix():
     print(z[1])
     print(z[2])
     print(z[3])
+
+print_matrix()
 
 def rotate_counterclockwise():
     z[0][0], z[3][0] = z[3][0], z[0][0]
@@ -124,9 +63,3 @@ while play:
     else:
         print("Terima kasih!")
         play = False
-# size = 4
-# max = 3 = size - 1
-# max - 1 = 2
-# max - 2 = 1
-# etc
-
